b6c7bd2fd578869d742891f178fb54cfee835f2f
jermaine berkley <jermaineberkley8@gmail.com>
2017-01-28 23:07:49 -0800

    added

---
+++ Assets/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    private Rigidbody2D player_rigidbody;
+    
+    [SerializeField]
+    private float player_speed;
+
+    // Use this for initialization
+    void Start () {
+        player_rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void movement_handler(float horizontal) {
+        player_rigidbody.velocity = new Vector2(horizontal * player_speed, player_rigidbody.velocity.y);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        movement_handler(horizontal);
+	}
+}
